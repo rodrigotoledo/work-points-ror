020ba474b065accb62ac03fb7402c2c5286b7f37
Rodrigo Toledo <rodrigo@Rodrigos-MacBook-Pro.local>
2022-12-07 09:22:15 -0300

    api endpoint form of state of work with select

--- app/models/state_of_work.rb
+++ app/models/state_of_work.rb
@@ -1,3 +1,6 @@
 class StateOfWork < ApplicationRecord
+  enum state: %i[started launching ending_work]
   belongs_to :user
+
+  validates :state, :state_at, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  namespace :api do
+    resources :state_of_works
+  end
   resources :state_of_works
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
